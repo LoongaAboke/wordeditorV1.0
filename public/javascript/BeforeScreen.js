3a0a43c0995ede3a80a30fd17c3bb1e684a9d42d
F4reo G Dev <71158689+LoongaAboke@users.noreply.github.com>
2022-02-28 01:29:07 +0000

    this is good

--- public/javascript/BeforeScreen.js
+++
@@ -1,9 +0,0 @@
-let AnimLogo = document.querySelector(".LHLogo01");
-
-let AnimBckgorund = document.querySelector(".LHolder01"); 
-
-AnimBckgorund.style.display = "block";
-
-window.onload = () =>{
-	AnimBckgorund.style.display = "none"; 
-}
